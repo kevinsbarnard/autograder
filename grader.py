035efbc703e9057bc23ee9665651c6495e8f5cf9
Kevin Barnard <kbarnard@mbari.org>
2019-09-24 18:28:48 -0600

    Added warning for non asm files, fixed bug with filename spaces

--- grader.py
+++ grader.py
@@ -6,10 +6,18 @@ import os
 
 
 mars_jar = 'Mars4_5.jar'
+extension = 'asm'
 
 
 def main(directory, n):
-    files = glob.glob(os.path.join(directory, '*asm'))
+    all_files = glob.glob(os.path.join(directory, '*'))
+    files = glob.glob(os.path.join(directory, '*{}'.format(extension)))
+    non_asm_files = list(set(all_files) - set(files))
+    if non_asm_files:
+        print('WARNING - {} file(s) found in directory without extension {}:'.format(len(non_asm_files), extension))
+        for non_asm in non_asm_files:
+            print(os.path.basename(non_asm))
+        print('--/!\\--')
     files.sort()
     filenames = [file.split(os.sep)[-1] for file in files]
     base_command = 'java -jar'
@@ -17,7 +25,7 @@ def main(directory, n):
     for i, file in enumerate(files):
         print(('[LATE] ' if '_late_' in file else '') + filenames[i].split('_')[0])
         for _ in range(n):
-            exit_code = int(os.system(' '.join([base_command] + arguments + [file])))
+            exit_code = int(os.system(' '.join([base_command] + arguments + ["\"{}\"".format(file)])))
             if exit_code != 0:
                 print('Exited with code', exit_code)
         print('-----')
